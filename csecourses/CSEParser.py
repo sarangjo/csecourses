efbe4c6d623b332ff345816c49101ff7ce78968f
Sarang Joshi <sarangj@msn.com>
2015-11-08 03:24:48 -0800

    Finished parser, added d3, started tutorial

---
+++ csecourses/CSEParser.py
@@ -0,0 +1,104 @@
+import csv
+import re
+from html.parser import HTMLParser
+
+__author__ = 'Sarang'
+
+
+# MyHTMLParser extends the HTMLParser class
+class MyHTMLParser(HTMLParser):
+    def error(self, message):
+        pass
+
+    def __init__(self):
+        super().__init__()
+        # parsing fields
+        self.inDesc = False
+        self.inName = False
+        self.inP = False
+        self.inTitle = False
+        # other fields
+        self.nOfP = 0
+        self.classes = []
+        self.cseClass = CSEClass()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a" and not self.inP:
+            # class code before paragraph
+            self.cseClass = CSEClass()
+            self.parse_code(attrs[0][1])
+        elif tag == "p":
+            # entered a new class paragraph
+            self.inP = True
+        elif tag == "b" and self.inP:
+            # set name
+            self.inName = True
+
+    def handle_data(self, data):
+        if self.inName:
+            self.parse_name(data)
+        elif self.inDesc:
+            self.parse_description(data)
+
+    def handle_endtag(self, tag):
+        if tag == "p" and self.inP:
+            # Finished reading a single class
+            self.classes.append(self.cseClass)
+            self.nOfP += 1
+            self.inP = False
+        elif tag == "b" and self.inP:
+            # set name of class
+            self.inName = False
+            if not self.inDesc:
+                # start of description paragraph
+                self.inDesc = True
+        elif tag == "end":
+            # finished file
+            print('Finished!')
+        elif tag == "a" and self.inDesc:
+            # finished description paragraph
+            self.inDesc = False
+
+    def parse_name(self, name):
+        """Parses the name of the class.
+        :param name:
+        """
+        self.cseClass.name = name
+
+    def parse_code(self, name):
+        code = re.findall(r'\d+', name)
+        self.cseClass.code = code[0]
+
+    def parse_description(self, desc):
+        self.cseClass.description = desc
+
+
+class CSEClass(object):
+    def __init__(self):
+        super().__init__()
+        self.code = 0
+        self.name = ""
+        self.description = ""
+
+    @property
+    def __str__(self):
+        s = ("CODE: " + str(self.code))
+        s += ("\nNAME: " + self.name)
+        s += ("\nDESC: " + self.description)
+        return s
+
+
+f = open('cse.html')
+s = f.read()
+
+parser = MyHTMLParser()
+parser.feed(s)
+
+# print(parser.classes[4].description)
+
+csv_file = open('csecourses.csv', 'w', newline='')
+csv_writer = csv.writer(csv_file)
+
+csv_writer.writerow(['Code', 'Name', 'Description'])
+for x in parser.classes:
+    csv_writer.writerow([x.code, x.name, x.description])
